ca86e8d817e65f140ea88fb0e7f1eecae8a8161d
Rohit9848 <39462535+Rohit9848@users.noreply.github.com>
2023-06-09 11:02:47 -0400

    Task1& Task2
    Create Assessment Form (UI/backend insert)
    
    View Assessment List(Get list/delete)

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -3,7 +3,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { SiteWrapper } from './components';
 import { DashboardBulletin } from './pages/Dashboard/DashboardBulletin';
 import { NewAssessment } from './pages/Assessments/NewAssessment.jsx';
-import { AssessmentList } from './pages/Assessments/AssessmentList';
+import { AssessmentList } from './pages/Assessments/AssessmentList.jsx';
+import { AssessmentListV2 } from './pages/Assessments/AssessmentListV2.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const router = createBrowserRouter([
@@ -15,7 +16,14 @@ const router = createBrowserRouter([
     element: <NewAssessment />,
     path: `/assessment/new`,
   },
-
+  {
+    element: <AssessmentList />,
+    path: `/assessment/list`,
+  },
+  {
+    element: <AssessmentListV2 />,
+    path: `/assessment/list2`,
+  },
 ]);
 
 const App = () => <SiteWrapper>
--- packages/client/src/pages/Assessments/AssessmentList.jsx
+++ packages/client/src/pages/Assessments/AssessmentList.jsx
@@ -1,23 +1,72 @@
 import React, { useEffect, useState } from 'react';
+import ReactTable from 'react-table-6';
+import 'react-table-6/react-table.css';
+
 import { AssessmentService } from '../../services/AssessmentService';
 
 export const AssessmentList = () => {
   const [ assessments, setAssessments ] = useState([]);
+  const [ instrumentType, setInstrumentType ] = useState(``);
 
-  // fetch all assessments using the AssessmentService.getList function from OCAT/client/services/AssessmentService.js
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: `Cat Id`,
+        accessor: `id`,
+      }, {
+        Header: `Cat Name`,
+        accessor: `catName`,
+      }, {
+        Header: `Score`,
+        accessor: `score`,
+      }, {
+        Header: `Risk Level`,
+        accessor: `riskLevel`,
+      }, {
+        Header: `Cat Date Of Birth`,
+        accessor: `catDateOfBirth`,
+      }, {
+        Header: `Created At`,
+        accessor: `updatedAt`,
+      }, {
+        Cell: ({ row }) =>
+          <button style={{
+            backgroundColor: `red`,
+            color: `white`,
+            cursor: `pointer`,
+          }} onClick={() => handleDelete(row)}>Delete</button>,
+        disableSortBy: true,
+      },
+    ], [],
+  );
+
   useEffect(() => {
     const fetchAssessments = async () => {
-      setAssessments(await AssessmentService.getList());
+      const assessmentResponse = await AssessmentService.getList();
+      setAssessments(assessmentResponse);
+      setInstrumentType(assessmentResponse[0].instrumentType);
     };
     fetchAssessments();
   }, []);
 
+  const handleDelete = async (row) => {
+    await AssessmentService.deleteAssessment(row._original.id);
+    setAssessments((prevAssessments) =>
+      prevAssessments.filter((assessment) => assessment.id !== row._original.id));
+  };
+
   return (
-    <div>
-      {/*
-          List goes here
-          Please use the library react-table https://www.npmjs.com/package/react-table
-      */}
+    <div style={{ textAlign: `center` }}>
+      <h3 style={{ display: `inline`, marginRight: `10px` }}>Cat Assessments List -</h3>
+      <h4 style={{ display: `inline` }}>{instrumentType}</h4>
+      <ReactTable
+        data={assessments}
+        columns={columns}
+        defaultPageSize={10}
+        pageSizeOptions={[ 5, 10, 16 ]}
+        filterable={true}
+      />
     </div>
   );
+
 };
